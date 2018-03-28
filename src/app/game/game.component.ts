24f8e90484f25d359ac20f22941a522f09a22fe5
Unknown <utejaswani@gmail.com>
2018-03-28 00:05:08 -0400

    Modified Game component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,7 +1,6 @@
-
-
 import { Component, OnInit } from '@angular/core';
-import {Game} from '../models/game';
+import { Game, User, Quote } from '../models/game';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -9,15 +8,27 @@ import {Game} from '../models/game';
 })
 export class GameComponent implements OnInit {
 
-  Model=new Game();
+    Model = new Game();
+    Me = new User();
 
-  constructor() { }
+  constructor() {
+    this.Me.Name = "Tejaswini Utlapalli"
+  }
 
   ngOnInit() {
   }
 
-submitQuote(e: MouseEvent,text: string, PlayerName: string){
-e.preventDefault();
-this.Model.PlayedQuotes.push({Text: text,PlayerName: PlayerName, Chosen: false });
+  submitQuote(e: MouseEvent, text: string){
+    e.preventDefault();
+
+    if(this.MyPlayedQuote()) return;
+
+    this.Model.PlayedQuotes.push({ Text: text, PlayerName: this.Me.Name, Chosen: false });
+    this.Model.MyQuotes.splice( this.Model.MyQuotes.indexOf(text), 1 );
   }
+
+  MyPlayedQuote(): Quote | null {
+    return this.Model.PlayedQuotes.find( x => x.PlayerName == this.Me.Name );
+  }
+
 }
--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,18 +1,17 @@
 
-
 export class Game {
-    MyQuotes:string[]=["Great Minds think alikebut...","Life is beautiful","Nothing is impossible"];
-    Palyers:User[]=[{ Name: 'Tejaswini'}];
-    Picture:string='http://f8rentals.com/wp-content/uploads/2015/03/IMG_0067ft.jpg?189db0';
-    PlayedQuotes:Quote[]=[{ Text: "Gret minds think alike, but ...",PlayerName:'Tejaswini', Chosen:false}];
-
-};
+    MyQuotes: string[] = ["Great Minds think alikebut...","Life is beautiful","Nothing is impossible"];
+    Players: User[] = [ { Name: 'Tejaswini' }];
+    Picture: string = 'http://f8rentals.com/wp-content/uploads/2015/03/IMG_0067ft.jpg?189db0';
+    PlayedQuotes: Quote[] = [];
+}
 
 export class User {
-    Name:string;
-};
+    Name: string;
+}
+
 export class Quote {
- Text:string;
- PlayerName:string;
- Chosen:boolean=false;
+    Text: string;
+    PlayerName: string;
+    Chosen: boolean = false;
 }
