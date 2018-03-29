1d73c2a267c187c33b82cd7914df3f4391da5758
Unknown <utejaswani@gmail.com>
2018-03-29 10:38:54 -0400

    Game Component updated.

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,9 +1,10 @@
 
 export class Game {
     MyQuotes: string[] = ["Great Minds think alikebut...","Life is beautiful","Nothing is impossible"];
-    Players: User[] = [ { Name: 'Tejaswini' }];
+    Players: User[] = [ { Name: 'Tejaswini' },{ Name: 'Surekha' },{ Name: 'Keerthi' },{ Name: 'Roja' }];
     Picture: string = 'http://f8rentals.com/wp-content/uploads/2015/03/IMG_0067ft.jpg?189db0';
-    PlayedQuotes: Quote[] = [];
+    PlayedQuotes: Quote[] = [{Text: "That's fake news", PlayerName: 'Surekha', Chosen:false}];
+    Dealer:string="Tejaswini";
 }
 
 export class User {
